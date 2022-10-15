1507280d71d1a8211b77ff82d6793bda59175aa9
Congrak <rodricelis@gmail.com>
2022-10-15 15:39:30 +0000

    Finish

--- src/app.js
+++ src/app.js
@@ -8,4 +8,21 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let domain = [];
+
+  function gene() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+
+    for (let p = 0; p < pronoun.length; p++) {
+      for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+          domain.push(pronoun[p] + adj[a] + noun[n] + ".com");
+        }
+      }
+    }
+    return domain;
+  }
+  document.querySelector("#gen").innerHTML = gene().join("\r");
 };
